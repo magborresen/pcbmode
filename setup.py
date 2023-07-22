3cfaf542231e10aa8ee6499fffb36ef06d06d403
Magnus Borressen <mb@magnusborresen.com>
2023-07-22 10:22:45 +0200

    changed build system from old setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,4 @@
-from setuptools import setup, find_namespace_packages
+import setuptools
 
-setup(
-    name = "pcbmode",
-    description = "A printed circuit board design tool with a twist",
-    long_description = "",
-    version = "5.0.0a1",
-    license = "GPL 3.0",
-    url = "https://pcbmode.com",
-
-    author = "Saar Drimer",
-    author_email = "saardrimer@gmail.com",
-    keywords = "pcb svg eda pcbmode",
-
-    python_requires = ">=3.7.0",
-
-    packages=find_namespace_packages(include=['pcbmode.*']),
-
-    test_suite='tests',
-
-    install_requires = [
-        'lxml>=4.4.0',
-        'pyparsing>=2.4.2'
-    ],
-
-    package_data = {
-        'pcbmode': [
-            'stackups/*.json',
-            'styles/*/*.json',
-            'fonts/*.svg',
-            'pcbmode_config.json'
-        ],
-    },
-
-    entry_points={
-        'console_scripts': [
-            'pcbmode = pcbmode.pcbmode:main'
-        ]
-    }
-)
+if __name__ == "__main__":
+    setuptools.setup()
